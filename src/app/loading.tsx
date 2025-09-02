296b610321e62d670a0273808ec2c300ab0531ce
dasilva218 <dasilvawil218@gmail.com>
2025-09-02 13:46:40 +0100

    setup du projet

---
+++ src/app/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="animate-pulse flex space-x-4">{/* Your loading skeleton here */}</div>
+}
